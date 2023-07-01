82f2cc37a19caa1801a628e8d0ca320299b47dcf
Yuşa  <yusasarisoy@icloud.com>
2023-07-01 18:14:20 +0200

    Update colors of the app

--- Talkaid/Utilities/Enums/ColorTheme.swift
+++ Talkaid/Utilities/Enums/ColorTheme.swift
@@ -2,17 +2,32 @@ import SwiftUI
 
 enum ColorTheme {
   case ghostWhite
-  case pianoBlack
+  case accentColor
   case echoBlue
+  case micColor
+  case chatInputBackground
+  case vividBlue
+  case blue
+  case purple
 
   var color: Color {
     switch self {
     case .ghostWhite:
       return Color("GhostWhite")
-    case .pianoBlack:
-      return Color("PianoBlack")
+    case .accentColor:
+      return Color("AccentColor")
     case .echoBlue:
       return Color("EchoBlue")
+    case .micColor:
+      return Color("MicColor")
+    case .chatInputBackground:
+      return Color("ChatInputBackground")
+    case .vividBlue:
+      return Color("VividBlue")
+    case .blue:
+      return Color("Blue")
+    case .purple:
+      return Color("Purple")
     }
   }
 }

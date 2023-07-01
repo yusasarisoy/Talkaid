bd2bdbcd9e7357dd493e72d710283329e50594a2
Yuşa  <yusasarisoy@icloud.com>
2023-07-01 10:18:24 +0200

    Initial commit

--- Talkaid/ContentView.swift
+++ Talkaid/ContentView.swift
@@ -1,26 +1,19 @@
-//
-//  ContentView.swift
-//  Talkaid
-//
-//  Created by Yuşa on 01.07.23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Talkaid/TalkaidApp.swift
+++ Talkaid/TalkaidApp.swift
@@ -1,17 +1,10 @@
-//
-//  TalkaidApp.swift
-//  Talkaid
-//
-//  Created by Yuşa on 01.07.23.
-//
-
 import SwiftUI
 
 @main
 struct TalkaidApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

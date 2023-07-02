39cf34f2bf71e09285873dbb0fbf8de1676ce4db
Yuşa  <yusasarisoy@icloud.com>
2023-07-02 13:05:37 +0200

    Add a View extension called cornerRadius for chat bubbles

--- Talkaid/Utilities/Extensions/View.swift
+++ Talkaid/Utilities/Extensions/View.swift
@@ -4,4 +4,11 @@ extension View {
   func frame(size: CGFloat) -> some View {
     frame(width: size, height: size)
   }
+
+  func cornerRadius(_ radius: CGFloat, sentByUser: Bool) -> some View {
+    let corners: UIRectCorner = sentByUser
+    ? [.bottomLeft, .topLeft, .topRight]
+    : [.bottomRight, .topLeft, .topRight]
+    return clipShape(RoundedCorner(radius: radius, corners: corners))
+  }
 }

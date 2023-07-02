3dc775089cc3e97ac054cbb23fbeec88e703dd1f
Yuşa  <yusasarisoy@icloud.com>
2023-07-02 13:08:17 +0200

    Add RoundedCorner for chat bubbles

---
+++ Talkaid/Utilities/Structs/RoundedCorner.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RoundedCorner: Shape {
+  var radius: CGFloat = 10
+  var corners: UIRectCorner = .allCorners
+
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    return Path(path.cgPath)
+  }
+}
